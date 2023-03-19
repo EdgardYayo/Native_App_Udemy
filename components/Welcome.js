7b928d8dcf44396c182bda4e656596aa1cf6f274
EdgardYayo <allan.pazos@uabc.edu.mx>
2023-03-18 17:39:54 -0700

    starting a new App

---
+++ components/Welcome.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { View, Text } from "react-native";
+
+
+const Welcome = ({name}) => {
+    return (
+      <View>
+        <Text>Hello {name}!!</Text>
+      </View>
+    )
+}
+
+export default Welcome;
